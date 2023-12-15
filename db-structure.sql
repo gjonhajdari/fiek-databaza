bc39d5433fddc3349fe0c4b2d16a4e470beac510
Gjon Hajdari <gjonhajdari1@gmail.com>
2023-12-15 10:46:00 +0100

    Created stucture for student table

---
+++ db-structure.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE application;
+
+-- student table
+CREATE TABLE application.student (
+  student_id INT(9) NOT NULL,
+  first_name NVARCHAR(50) NOT NULL,
+  last_name NVARCHAR(50) NOT NULL,
+  gender CHAR(1) NOT NULL,
+  birthdate DATE NOT NULL,
+  marriage_status VARCHAR(8) NOT NULL,
+  address NVARCHAR(100) NOT NULL,
+  phone_number VARCHAR(20) NOT NULL,
+  email_address VARCHAR(320) NOT NULL,
+  username VARCHAR(32) NOT NULL,
+  password CHAR(64) NOT NULL,
+  profile_picture VARCHAR(100) NULL,
+  skills VARCHAR(300) NULL,
+  hobbies VARCHAR(300) NULL,
+  books_movies_music VARCHAR(300) NULL,
+  description VARCHAR(300) NULL
+)
