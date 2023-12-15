be7dca274069de068ef0e411579a0c982587562a
Gjon Hajdari <gjonhajdari1@gmail.com>
2023-12-15 11:27:42 +0100

    Created structure for the education table

--- db-structure.sql
+++ db-structure.sql
@@ -4,7 +4,7 @@ USE application;
 
 -- student table
 CREATE TABLE IF NOT EXISTS student (
-  student_id INT(9) NOT NULL,
+  student_id INT NOT NULL,
   first_name NVARCHAR(50) NOT NULL,
   last_name NVARCHAR(50) NOT NULL,
   gender CHAR(1) NOT NULL,
@@ -20,5 +20,20 @@ CREATE TABLE IF NOT EXISTS student (
   hobbies VARCHAR(300) NULL,
   books_movies_music VARCHAR(300) NULL,
   description VARCHAR(300) NULL,
-  PRIMARY KEY(student_id)
+  PRIMARY KEY (student_id)
+);
+
+CREATE TABLE IF NOT EXISTS education (
+  education_id INT NOT NULL,
+  student_id INT NOT NULL,
+  institution NVARCHAR(30) NOT NULL,
+  field_of_study NVARCHAR(50) NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE NULL,
+  diploma_file VARCHAR(100) NULL,
+  degree VARCHAR(100) NULL,
+  study_area VARCHAR(50) NULL,
+  mentor NVARCHAR(300) NULL,
+  PRIMARY KEY (education_id),
+  FOREIGN KEY (student_id) REFERENCES student(student_id)
 );
