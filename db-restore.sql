95d62aa897a13762b95aa358d43c938ee0f85b5d
Gjon Hajdari <gjonhajdari1@gmail.com>
2023-12-15 11:31:51 +0100

    Added clearing and deleting for the education table

--- db-restore.sql
+++ db-restore.sql
@@ -1,5 +1,10 @@
 -- Clearing all the fields
 TRUNCATE TABLE application.student;
+TRUNCATE TABLE application.education;
 
 -- Deleting the tables
 DROP TABLE application.student;
+DROP TABLE application.education;
+
+-- Deleting the entire database
+DROP DATABASE application;
