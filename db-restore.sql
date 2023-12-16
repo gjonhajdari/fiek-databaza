cbb9b1117df78a525b6fb29ea0f88e47f9fe4c26
Gjon Hajdari <gjonhajdari1@gmail.com>
2023-12-16 21:24:21 +0100

    Added clearing and deleting for the remaining tables

--- db-restore.sql
+++ db-restore.sql
@@ -1,12 +1,30 @@
--- clearing all the fields
-truncate table application.student;
-truncate table application.education;
-truncate table application.project;
+-- clearing all the table fields
+truncate table if exists application.student;
+truncate table if exists application.education;
+truncate table if exists application.project;
+truncate table if exists application.experience;
+truncate table if exists application.company;
+truncate table if exists application.post;
+truncate table if exists application.comment;
+truncate table if exists application.repost;
+truncate table if exists application.like;
+truncate table if exists application.volunteering;
+truncate table if exists application.language;
+truncate table if exists application.saved_post;
 
 -- deleting the tables
-drop table application.student;
-drop table application.education;
-drop table application.project;
+drop table if exists application.student;
+drop table if exists application.education;
+drop table if exists application.project;
+drop table if exists application.experience;
+drop table if exists application.company;
+drop table if exists application.post;
+drop table if exists application.comment;
+drop table if exists application.repost;
+drop table if exists application.like;
+drop table if exists application.volunteering;
+drop table if exists application.language;
+drop table if exists application.saved_post;
 
 -- deleting the entire database
-drop database application;
+drop database if exists application;
