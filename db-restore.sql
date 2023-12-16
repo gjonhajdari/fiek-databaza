972eb9512c3a6d70fb3d17bb8c816c85c6064ec0
Gjon Hajdari <gjonhajdari1@gmail.com>
2023-12-16 21:16:50 +0100

    Added clearing and deleting for project table

--- db-restore.sql
+++ db-restore.sql
@@ -1,10 +1,12 @@
 -- Clearing all the fields
 TRUNCATE TABLE application.student;
 TRUNCATE TABLE application.education;
+TRUNCATE TABLE application.project;
 
 -- Deleting the tables
 DROP TABLE application.student;
 DROP TABLE application.education;
+DROP TABLE application.project;
 
 -- Deleting the entire database
 DROP DATABASE application;
