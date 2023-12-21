82ccd0e420007d2ec9355c9d5865734ce2b210e9
Gjon Hajdari <gjonhajdari1@gmail.com>
2023-12-21 23:13:19 +0100

    Added query 3

--- db-queries.sql
+++ db-queries.sql
@@ -7,6 +7,7 @@ LEFT JOIN applied a ON a.student_id = s.student_id
 WHERE s.address LIKE "%Prishtina%"
 AND a.student_id IS NULL;
 
+
 -- 2.
 -- Cilët studentë (numrat unik, emrat dhe mbiemrat e tyre) dje dhe sot kanë komentuar dhe kanë shpërndarë (share) ndonjë pozitë?
 SELECT s.student_id, s.first_name, s.last_name
@@ -19,3 +20,13 @@ WHERE
   (DATE(r.reposted_at) = CURDATE() OR DATE(r.reposted_at) = CURDATE() - INTERVAL 1 DAY);
 
 select * from application.`student`;
+
+
+-- 3.
+-- Paraqitni të gjitha pozitat që janë publikuar këtë muaj në qytetin e Prishtinës dhe për të cilat pozita nuk ka asnjë studentë që ka aplikuar.
+SELECT p.* FROM post p
+LEFT JOIN applied a ON a.post_id = p.post_id
+WHERE p.location = "Prishtinë"
+AND MONTH(p.posted_at) = MONTH(NOW())
+AND YEAR(p.posted_at) = YEAR(NOW())
+AND a.student_id IS NULL;
