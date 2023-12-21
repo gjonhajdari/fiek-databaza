bb1bb4907a38f19e4933e3dee7adce0384377042
Gjon Hajdari <gjonhajdari1@gmail.com>
2023-12-21 14:03:25 +0100

    Created trigger for updating the like count

--- db-triggers.sql
+++ db-triggers.sql
@@ -1,15 +1,35 @@
-1_UpdateExperienceYearsOnInsert:
-CREATE TRIGGER UpdateExperienceYearsOnInsert
-AFTER INSERT ON EXPERIENCE
-FOR EACH ROW
-UPDATE STUDENT
-SET TotalExperienceYears = TotalExperienceYears + NEW.YearsOfExperience
-WHERE STUDENT.StudentID = NEW.StudentID;
+-- CREATE TRIGGER UpdateExperienceYearsOnInsert
+-- AFTER INSERT ON EXPERIENCE
+-- FOR EACH ROW
+-- UPDATE STUDENT
+-- SET TotalExperienceYears = TotalExperienceYears + NEW.YearsOfExperience
+-- WHERE STUDENT.StudentID = NEW.StudentID;
 
-2_PostLikesCounter:
-CREATE TRIGGER PostLikesCounter
-AFTER INSERT ON LIKES
+
+-- Trigger for updating like_count of the respective table when a new like is inserted
+DELIMITER //
+
+CREATE TRIGGER application.update_like_count
+AFTER INSERT ON `application`.`like`
 FOR EACH ROW
-UPDATE POST
-SET LikeCount = LikeCount + 1
-WHERE POST.PostID = NEW.PostID;
+BEGIN
+    IF NEW.post_id IS NOT NULL THEN
+        -- Update like_count for a post
+        UPDATE `application`.`post`
+        SET like_count = like_count + 1
+        WHERE post_id = NEW.post_id;
+    ELSEIF NEW.repost_id IS NOT NULL THEN
+        -- Update like_count for a repost
+        UPDATE `application`.`repost`
+        SET like_count = like_count + 1
+        WHERE repost_id = NEW.repost_id;
+    ELSEIF NEW.comment_id IS NOT NULL THEN
+        -- Update like_count for a comment
+        UPDATE `application`.`comment`
+        SET like_count = like_count + 1
+        WHERE comment_id = NEW.comment_id;
+    END IF;
+END;
+//
+
+DELIMITER ;
