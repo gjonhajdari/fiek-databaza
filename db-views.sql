9df697de1ae105f4bcb932ea085ef01dd34e9d5c
Gjon Hajdari <gjonhajdari1@gmail.com>
2023-12-22 13:35:23 +0100

    Created views for latest job postings and students along with their skills

--- db-views.sql
+++ db-views.sql
@@ -1,17 +1,39 @@
-1_StudentDetalisView:
-CREATE VIEW StudentDetailsView AS
-SELECT * FROM STUDENT
-JOIN EDUCATION ON STUDENT.StudentID = EDUCATION.StudentID
-JOIN EXPERIENCE ON STUDENT.StudentID = EXPERIENCE.StudentID;
+-- View that shows the most recent job postings
+CREATE VIEW `application`.`latest_posts` AS
+SELECT
+    p.post_id,
+    p.company_id,
+    c.company_name,
+    p.title,
+    p.description,
+    p.required_skills,
+    p.desired_skills,
+    p.job_type,
+    p.location,
+    p.posted_at,
+    p.application_deadline,
+    p.like_count
+FROM
+    post p
+INNER JOIN
+    company c ON p.company_id = c.company_id
+WHERE
+    p.application_deadline >= CURDATE()
+ORDER BY
+    p.posted_at DESC;
 
-2_ProjectsWithApplicantsView:
-CREATE VIEW ProjectsWithApplicantsView AS
-SELECT PROJECT.*, COUNT(APPLYING.StudentID) AS ApplicantCount
-FROM PROJECT
-LEFT JOIN APPLYING ON PROJECT.ProjectID = APPLYING.ProjectID
-GROUP BY PROJECT.ProjectID;
-
-
-
-
-
+-- View that shows students and a list of their registered skills
+CREATE VIEW `application`.`student_with_skills` AS
+SELECT
+    s.student_id,
+    s.first_name,
+    s.last_name,
+    s.username,
+    s.profile_picture,
+    GROUP_CONCAT(skill.skill_name ORDER BY skill.skill_name) AS skills
+FROM
+    student s
+LEFT JOIN
+    skill ON s.student_id = skill.student_id
+GROUP BY
+    s.student_id;
