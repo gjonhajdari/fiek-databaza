ee8757bf82b1f446ff699604ab3b6eb4b7706ee3
Gjon Hajdari <gjonhajdari1@gmail.com>
2023-12-23 21:00:12 +0100

    Formatted code to use 2 spaces instead of 4

--- db-storedprocedures.sql
+++ db-storedprocedures.sql
@@ -3,28 +3,28 @@ DELIMITER //
 
 CREATE PROCEDURE `application`.`GetStudentDetails`(IN studentId INT)
 BEGIN
-    SELECT
-        s.student_id,
-        s.first_name,
-        s.last_name,
-        e.education_id,
-        e.institution_name,
-        e.field_of_study,
-        e.start_date,
-        e.end_date,
-        ex.experience_id,
-        ex.title,
-        ex.company_name,
-        ex.start_date AS exp_start_date,
-        ex.end_date AS exp_end_date
-    FROM
-        student s
-    LEFT JOIN
-        education e ON s.student_id = e.student_id
-    LEFT JOIN
-        experience ex ON s.student_id = ex.student_id
-    WHERE
-        s.student_id = studentId;
+  SELECT
+    s.student_id,
+    s.first_name,
+    s.last_name,
+    e.education_id,
+    e.institution_name,
+    e.field_of_study,
+    e.start_date,
+    e.end_date,
+    ex.experience_id,
+    ex.title,
+    ex.company_name,
+    ex.start_date AS exp_start_date,
+    ex.end_date AS exp_end_date
+  FROM
+    student s
+  LEFT JOIN
+    education e ON s.student_id = e.student_id
+  LEFT JOIN
+    experience ex ON s.student_id = ex.student_id
+  WHERE
+    s.student_id = studentId;
 END //
 
 DELIMITER ;
@@ -34,8 +34,8 @@ DELIMITER //
 
 CREATE PROCEDURE `application`.`GetPostsByCompany`(IN companyId INT)
 BEGIN
-    SELECT * FROM post
-    WHERE company_id = companyId;
+  SELECT * FROM post
+  WHERE company_id = companyId;
 END //
 
 DELIMITER ;
