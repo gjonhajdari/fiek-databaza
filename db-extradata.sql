ba9e775bb4fc90164ff6eb9ad5b2efe0279bc96d
Gjon Hajdari <gjonhajdari1@gmail.com>
2023-12-23 21:01:25 +0100

    Fixed typo in extra data

--- db-extradata.sql
+++ db-extradata.sql
@@ -7,12 +7,14 @@ values
 (6, 3, "Test 3", "2023-12-21 14:30", 0),
 (8, 3, "Test", "2023-12-21 14:30", 221);
 
+
 insert into `application`.`repost`(post_id, student_id, content, reposted_at, like_count)
 values
 (1, 5, null, "2023-12-20 11:45", 3),
 (2, 5, null, "2023-12-20 14:20", 0),
 (2, 3, null, "2023-12-20 14:20", 0),
 (2, 3, null, "2023-12-20 14:20", 0);
+
 
 insert into `application`.`post`(company_id, title, description, required_skills, desired_skills, job_type, location, posted_at, application_deadline, like_count)
 values
@@ -22,27 +24,30 @@ values
 (7, "Data Scientist", "We are seeking a talented Data Scientist to analyze and interpret complex data sets. The ideal candidate should have a strong background in statistical analysis, machine learning, and data visualization.", "Proficiency in programming languages such as Python or R, Statistical analysis and hypothesis testing, Machine learning techniques", "Experience with big data technologies (Hadoop, Spark), Knowledge of natural language processing (NLP)", "Full-time", "Prishtinë", "2023-12-20 12:00", "2024-06-10", 811),
 (6, "Customer Support Specialist", "We are looking for a Customer Support Specialist to assist customers with product-related inquiries and provide exceptional service. The ideal candidate should have strong communication skills and a customer-centric approach.", "Excellent verbal and written communication, Problem-solving skills, Patience and empathy", "Experience with helpdesk software, Knowledge of the product or industry", "Full-time", "Prishtinë", "2023-12-05 10:15", "2024-08-05", 492);
 
+
 insert into `application`.`post`(company_id, title, description, required_skills, desired_skills, job_type, location, posted_at, application_deadline, like_count)
 values
 (4, "Title", "Desc", "Req", "Desired", "Full-time", "Prishtinë", "2023-10-04 09:30", "2024-02-20", 5),
 (7, "Title 2", "Desc", "Req", "Desired", "Full-time", "Prishtinë", "2023-11-12 09:30", "2024-01-05", 76);
+
 
 insert into `application`.`applied`(student_id, post_id, resume, applied_at)
 values
 (2, 15, "resume_1.pdf", "2023-12-20 08:45"),
 (2, 16, "resume_1.pdf", "2023-12-20 08:45");
 
+
 insert into `application`.`project`(student_id, project_name, start_date, end_date, institution_name, location, description, role, web_page)
 values
-(2, "Title", "2022-03-01", "2022-06-30", "Institutuin", "Prishtina, Kosovo", "Description",  "Role", "www.website.com"),
-(2, "Title", "2022-03-01", "2022-06-30", "Institutuin", "Prishtina, Kosovo", "Description",  "Role", "www.website.com"),
-(2, "Title", "2022-03-01", "2022-06-30", "Institutuin", "Prishtina, Kosovo", "Description",  "Role", "www.website.com"),
-(2, "Title", "2022-03-01", "2022-06-30", "Institutuin", "Prishtina, Kosovo", "Description",  "Role", "www.website.com"),
-(3, "Title", "2022-03-01", "2022-06-30", "Institutuin", "Prishtina, Kosovo", "Description",  "Role", "www.website.com"),
-(3, "Title", "2022-03-01", "2022-06-30", "Institutuin", "Prishtina, Kosovo", "Description",  "Role", "www.website.com"),
-(3, "Title", "2022-03-01", "2022-06-30", "Institutuin", "Prishtina, Kosovo", "Description",  "Role", "www.website.com"),
-(3, "Title", "2022-03-01", "2022-06-30", "Institutuin", "Prishtina, Kosovo", "Description",  "Role", "www.website.com"),
-(5, "Title", "2022-03-01", "2022-06-30", "Institutuin", "Prishtina, Kosovo", "Description",  "Role", "www.website.com"),
-(5, "Title", "2022-03-01", "2022-06-30", "Institutuin", "Prishtina, Kosovo", "Description",  "Role", "www.website.com"),
-(5, "Title", "2022-03-01", "2022-06-30", "Institutuin", "Prishtina, Kosovo", "Description",  "Role", "www.website.com"),
-(5, "Title", "2022-03-01", "2022-06-30", "Institutuin", "Prishtina, Kosovo", "Description",  "Role", "www.website.com");
+(2, "Title", "2022-03-01", "2022-06-30", "Institution", "Prishtina, Kosovo", "Description",  "Role", "www.website.com"),
+(2, "Title", "2022-03-01", "2022-06-30", "Institution", "Prishtina, Kosovo", "Description",  "Role", "www.website.com"),
+(2, "Title", "2022-03-01", "2022-06-30", "Institution", "Prishtina, Kosovo", "Description",  "Role", "www.website.com"),
+(2, "Title", "2022-03-01", "2022-06-30", "Institution", "Prishtina, Kosovo", "Description",  "Role", "www.website.com"),
+(3, "Title", "2022-03-01", "2022-06-30", "Institution", "Prishtina, Kosovo", "Description",  "Role", "www.website.com"),
+(3, "Title", "2022-03-01", "2022-06-30", "Institution", "Prishtina, Kosovo", "Description",  "Role", "www.website.com"),
+(3, "Title", "2022-03-01", "2022-06-30", "Institution", "Prishtina, Kosovo", "Description",  "Role", "www.website.com"),
+(3, "Title", "2022-03-01", "2022-06-30", "Institution", "Prishtina, Kosovo", "Description",  "Role", "www.website.com"),
+(5, "Title", "2022-03-01", "2022-06-30", "Institution", "Prishtina, Kosovo", "Description",  "Role", "www.website.com"),
+(5, "Title", "2022-03-01", "2022-06-30", "Institution", "Prishtina, Kosovo", "Description",  "Role", "www.website.com"),
+(5, "Title", "2022-03-01", "2022-06-30", "Institution", "Prishtina, Kosovo", "Description",  "Role", "www.website.com"),
+(5, "Title", "2022-03-01", "2022-06-30", "Institution", "Prishtina, Kosovo", "Description",  "Role", "www.website.com");
