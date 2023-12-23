b89a9a343a1fc2645481655784b600312d7e9029
Gjon Hajdari <gjonhajdari1@gmail.com>
2023-12-23 18:25:06 +0100

    Added query 5

--- db-queries.sql
+++ db-queries.sql
@@ -37,16 +37,37 @@ AND a.student_id IS NULL;
 SELECT s.*
 FROM student s
 LEFT JOIN (
-    SELECT student_id
-    FROM applied
-    WHERE DATE(applied_at) = CURDATE()
+  SELECT student_id
+  FROM applied
+  WHERE DATE(applied_at) = CURDATE()
 ) applications_today ON s.student_id = applications_today.student_id
 LEFT JOIN (
-    SELECT student_id
-    FROM applied
-    WHERE DATE(applied_at) = CURDATE() - INTERVAL 1 DAY
-    GROUP BY student_id
-    HAVING COUNT(*) >= 2
+  SELECT student_id
+  FROM applied
+  WHERE DATE(applied_at) = CURDATE() - INTERVAL 1 DAY
+  GROUP BY student_id
+  HAVING COUNT(*) >= 2
 ) applications_yesterday ON s.student_id = applications_yesterday.student_id
 WHERE applications_yesterday.student_id IS NOT NULL
   AND applications_today.student_id IS NULL;
+
+
+-- 4.
+-- Listoni top 5 kompanitë/institucionet me numër maksimal të pozitave të shpallura në dy muajt e fundit. Lista të paraqes të dhënat e kompanive/institucioneve (emrin, qytetin dhe email-in) duke përfshirë edhe numrin e pozitave të shpallura.
+SELECT
+  c.company_id,
+  c.company_name,
+  a.city,
+  c.email_address,
+  COUNT(p.post_id) AS post_count
+FROM
+  company c
+LEFT JOIN
+  post p ON c.company_id = p.company_id
+LEFT JOIN
+  address a ON c.company_id = a.company_id
+WHERE
+  p.posted_at >= CURDATE() - INTERVAL 2 MONTH
+GROUP BY c.company_id, c.company_name, a.city, c.email_address
+ORDER BY post_count DESC
+LIMIT 5;
